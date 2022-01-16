d62bc3dddfc90a47e3effae064d7320ef9cc2bc4
JOJOK63 <krariajordan@gmail.com>
2022-01-16 19:44:20 +0100

    correct

--- src/Entity/Advert.php
+++ src/Entity/Advert.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AdvertRepository::class)]
-abstract class Advert
+class Advert
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
